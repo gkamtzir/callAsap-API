c16598d4f3081f7812e1bd19010c8d5573027858
George Kamtziridis <georgekam96@gmail.com>
2017-08-18 01:04:12 +0300

    The required endpoints are ready

--- routes/api.js
+++ routes/api.js
@@ -23,11 +23,39 @@ apiRouter.route('/country/:countryName')
     .get(function(req, res, next) {
 
         var countryParams = req.params.countryName.slice(0, 1).toUpperCase() + req.params.countryName.toLowerCase().slice(1);
-        
+
         country.find({name: countryParams}, '-_id', function(err, country) {
 
             if (err) throw err;
             res.json(country);
+
+        });
+
+    });
+
+apiRouter.route('/country/:countryName/emergencies')
+    .get(function(req, res, next) {
+
+        var countryParams = req.params.countryName.slice(0, 1).toUpperCase() + req.params.countryName.toLowerCase().slice(1);
+
+        country.findOne({name: countryParams}, '-_id', function(err, country) {
+
+            if (err) throw err;
+            res.json(country.emergencyNumbers);
+
+        });
+
+    });
+
+apiRouter.route('/country/:countryName/languages')
+    .get(function(req, res, next) {
+
+        var countryParams = req.params.countryName.slice(0, 1).toUpperCase() + req.params.countryName.toLowerCase().slice(1);
+
+        country.findOne({name: countryParams}, '-_id', function(err, country) {
+
+            if (err) throw err;
+            res.json(country.languages);
 
         });
 
