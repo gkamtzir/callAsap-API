68ba5491d49833839ab36da17631b1ea90d6fa1e
George Kamtziridis <georgekam96@gmail.com>
2018-07-27 12:01:29 +0300

    Changed endpoints' names

--- app.js
+++ app.js
@@ -36,8 +36,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/api', api);
+app.use('/callasap', index);
+app.use('/callasap/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
